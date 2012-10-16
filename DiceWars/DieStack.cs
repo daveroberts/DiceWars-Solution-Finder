8e32105c8416898878225aa8e63103493db04e8d
Dave Roberts <dave.a.roberts@gmail.com>
2012-10-15 21:54:27 -0400

    init

---
+++ DiceWars/DieStack.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiceWars
+{
+	class DieStack
+	{
+		private int _numDie;
+		private int[] _values;
+		public DieStack(int numDie)
+		{
+			_numDie = numDie;
+			_values = new int[_numDie];
+			this.reset();
+		}
+		public bool hasNext()
+		{
+			for (int i = 0; i < _numDie; i++)
+			{
+				if (_values[i] != 6)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+		public bool next()
+		{
+			for (int i = 0; i < _numDie; i++)
+			{
+				if (_values[i] < 6)
+				{
+					_values[i]++;
+					return true;
+				}
+				else
+				{
+					_values[i] = 1;
+				}
+			}
+			return false;
+		}
+		public int sum
+		{
+			get
+			{
+				int total = 0;
+				for (int i = 0; i < _numDie; i++)
+				{
+					total += _values[i];
+				}
+				return total;
+			}
+		}
+		public bool isBetter(DieStack d)
+		{
+			//Console.WriteLine(this.sum + " versus " + d.sum);
+			return (this.sum > d.sum);
+		}
+		public void reset()
+		{
+			for (int i = 0; i < _numDie; i++)
+			{
+				_values[i] = 1;
+			}
+		}
+		public static double matchup(int d1, int d2)
+		{
+			DieStack ds1 = new DieStack(d1);
+			DieStack ds2 = new DieStack(d2);
+			long numTrials = 0;
+			long ds1Wins = 0;
+			do
+			{
+				do
+				{
+					numTrials++;
+					if (ds1.isBetter(ds2))
+					{
+						ds1Wins++;
+					}
+				}
+				while (ds2.next());
+			}
+			while (ds1.next());
+			return (double)ds1Wins / (double)numTrials;
+		}
+	}
+}
---
+++ DiceWars/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiceWars
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for (int left = 1; left <= 8; left++)
+			{
+				for (int right = 1; right <= 8; right++)
+				{
+					double num = DieStack.matchup(left, right);
+					string percentage = string.Format("{0:0.000%}", num);
+					Console.WriteLine(left + " vs " + right + ": " + percentage);
+				}
+			}
+			Console.ReadLine();
+		}
+	}
+}
